dc5a05e44322d7b3a5a7411e3e6ce6b4c9af3903
Nicholas Rabow <nicholasrabow@Nicholass-Mac-Studio.local>
2022-05-05 10:41:58 -0600

    init web app

---
+++ web_app/src/components/Footer.js
@@ -0,0 +1,13 @@
+import "../index.css";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <p>
+        <small>Nicholas Rabow &copy; 2022</small>
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
